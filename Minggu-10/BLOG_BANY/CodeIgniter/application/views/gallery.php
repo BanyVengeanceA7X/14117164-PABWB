ae42e3e530e83b200aa4c979597623d6b546711d
BanyMajesty <50118567+BanyVengeanceA7X@users.noreply.github.com>
2019-11-17 22:58:01 +0700

    codingan

---
+++ Minggu-10/BLOG_BANY/CodeIgniter/application/views/gallery.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <div class="footer">&copy; Copyright Achmad Bany Majesty</div>
+    <div class="gallery">
+        <a target="_blank" href="v4.jpg">
+            <img src="Asset/galery/v4.jpg" alt="Superbike" width="400" height="400">
+        </a>
+        
+    <div class="gallery">
+        <a target="_blank" href="1.jpg">
+            <img src="Asset/galery/1.jpg" alt="Superbike" width="600" height="400">
+        </a>
+    </div>
+
+    <div class="gallery">
+        <a target="_blank" href="2.jpg">
+            <img src="Asset/galery/2.jpg" alt="Superbike" width="600" height="400">
+        </a>
+    </div>
+
+    <div class="gallery">
+        <a target="_blank" href="3.jpg">
+            <img src="Asset/galery/3.jpg" alt="Superbike" width="600" height="400">
+        </a>
+    </div>
+
+    <div class="gallery">
+        <a target="_blank" href="5.jpg">
+            <img src="Asset/galery/5.jpg" alt="Superbike" width="600" height="400">
+        </a>
+    </div>
+</body>
+</html>
